9743adc3605e3167486e75bc0785092416aed797
Benjamin Sykes <bensykes12@gmail.com>
2015-08-24 13:46:39 -0700

    Added MainMenu Module to the Plugin

--- src/main/java/net/cubebeaters/cbshop/CBShop.java
+++ src/main/java/net/cubebeaters/cbshop/CBShop.java
@@ -1,14 +1,21 @@
 package net.cubebeaters.cbshop;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,7 +25,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  * they want with cash in a nice clean GUI
  *
  * @author Systemx86 (Bananna)
- * @version 0.5
+ * @version 0.7
  */
 public class CBShop extends JavaPlugin implements Listener {
 
@@ -66,9 +73,51 @@ public class CBShop extends JavaPlugin implements Listener {
     }
 
     /**
-     * 
+     *
+     * This Event is triggered when a player clickes an inventory Item and is
+     * used to control the GUI shop menu's
+     *
+     * @param event The Event that is triggered when an item is clicked within'
+     * a players inventory
+     */
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player p = (Player) event.getWhoClicked(); // The Player who clicked.
+        Inventory inv = event.getInventory(); // The Inventory that was used.
+        ItemStack clicked = event.getCurrentItem(); // The item that was clicked.
+
+        if (inv.getName().equals(mainMenu.getName())) {
+            if (null != clicked.getType()) {
+                switch (clicked.getType()) {
+                    case IRON_CHESTPLATE:
+                        event.setCancelled(true);
+                        createArmorMenu();
+                        p.openInventory(armorMenu);
+                        break;
+                    case IRON_SWORD:
+                        event.setCancelled(true);
+                        createWeaponMenu();
+                        p.openInventory(weaponMenu);
+                        break;
+                    case GOLD_SPADE:
+                        event.setCancelled(true);
+                        createToolsMenu();
+                        p.openInventory(toolMenu);
+                        break;
+                    case COAL:
+                        event.setCancelled(true);
+                        createOreMenu();
+                        p.openInventory(oreMenu);
+                        break;
+                }
+            }
+        }
+    }
+
+    /**
+     *
      * This method hooks into the servers default economy plugin.
-     * 
+     *
      * @return true if there is an Economy plugin present.
      */
     public boolean setupEconomy() {
@@ -100,12 +149,90 @@ public class CBShop extends JavaPlugin implements Listener {
         }
         return true;
     }
-    
+
     /**
      * Creates all the options In the main menu.
      */
     public void createMainMenu() {
-        
+        // Create Armor menu Item
+        createMenuItem(Material.IRON_CHESTPLATE, mainMenu, 0, ChatColor.GOLD + "Armor Shop", ChatColor.GREEN + "Click to open the Armor shop");
+        // Create Weapons menu Item
+        createMenuItem(Material.IRON_SWORD, mainMenu, 1, ChatColor.GOLD + "Armor Shop", ChatColor.GREEN + "Click to open the Weapon Shop");
+        // Create Tools menu Item
+        createMenuItem(Material.GOLD_SPADE, mainMenu, 2, ChatColor.GOLD + "Tools Shop", ChatColor.GREEN + "Click to open the Tools Shop");
+        // Create Ore menu Item
+        createMenuItem(Material.COAL, mainMenu, 3, ChatColor.GOLD + "Ore Shop", ChatColor.GREEN + "Click to open the Ore Shop");
+        // Create Exit Button
+        createMenuItem(Material.ENDER_CHEST, mainMenu, 17, ChatColor.BLUE + "Exit Shop", ChatColor.GREEN + "Click to exit the CBShop");
+    }
+
+    /**
+     * Creates all the contents of the Armor Shop
+     */
+    public void createArmorMenu() {
+        // TODO
+    }
+
+    /**
+     * Creates all the contents of the Weapons Shop
+     */
+    public void createWeaponMenu() {
+        // TODO
+    }
+
+    /**
+     * Creates all the contents of the Tools Shop
+     */
+    public void createToolsMenu() {
+        // TODO
+    }
+
+    /**
+     * Creates all the contents of the Ore Shop
+     */
+    public void createOreMenu() {
+        // TODO
+    }
+
+    /**
+     *
+     * Creates a purchasable Item for the shop.
+     *
+     * @param material The Item material to use.
+     * @param inv The inventory to add this item to.
+     * @param slot The Slot in the specified inventory which to add the item.
+     * @param amount The amount of the item to add.
+     * @param cost The cost for 'x' amount of the item.
+     */
+    public void createPurchaseItem(Material material, Inventory inv, int slot, int amount, int cost) {
+        String lore;
+
+        ItemStack item = new ItemStack(material);
+        item.setAmount(amount);
+        ItemMeta meta = item.getItemMeta();
+        lore = meta.getDisplayName() + " Cost: " + cost;
+        try {
+            List<String> Lore = new ArrayList();
+            Lore.add(lore);
+            meta.setLore(Lore);
+        } catch (NullPointerException ex) {
+            getLogger().log(Level.INFO, "Nothing entered for the lore of {0}.", meta.getDisplayName());
+        }
+        item.setItemMeta(meta);
+
+        inv.setItem(slot, item);
+    }
+
+    public void createMenuItem(Material material, Inventory inv, int Slot, String name, String lore) {
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        List<String> Lore = new ArrayList();
+        Lore.add(lore);
+        meta.setLore(Lore);
+        item.setItemMeta(meta);
+
+        inv.setItem(Slot, item);
     }
 
 }
